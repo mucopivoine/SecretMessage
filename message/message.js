56e3c5282bdf75d268ffa26bf6fbdfdd1cada933
mucopivoine <pivotmurerwa1@gmail.com>
2024-03-10 01:02:28 +0200

    message javascript

---
+++ message/message.js
@@ -0,0 +1,10 @@
+const secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,','learn','JavaScript'];
+secretMessage.pop();
+console.log(secretMessage.length);
+secretMessage.push('to','program');
+secretMessage[7] = 'right';
+console.log(secretMessage);
+secretMessage.shift();
+secretMessage.unshift('Programming');
+secretMessage.splice(6,5,'know');
+console.log(secretMessage.join(' '));
